356927de28dc5a34b41a1e5c70b12519fe55553e
Levin <llmar1000000@gmail.com>
2024-03-18 21:12:13 -0400

    Major refactor to enable very large board in order to simulate infinite board; very slow performance

--- src/GameOfLife.cpp
+++ src/GameOfLife.cpp
@@ -6,15 +6,15 @@
 
 bool get_val_or_false(const std::deque<std::deque<bool>>& p_grid, const int index_y, const int index_x)
 {
-	try
+	// assume p_grid sizes are the same
+	if ((index_y >= 0) && (index_x >= 0) && (index_y < static_cast<int>(p_grid.size())) && (index_x < static_cast<int>(p_grid[0].size())))
 	{
 		const bool val = p_grid.at(index_y).at(index_x);
 		return val;
 	}
-	catch (std::out_of_range&)
-	{
+	else
 		return false;
-	}
+
 }
 
 
@@ -24,31 +24,43 @@ bool random_bool() {
 	return distribution(generator);
 }
 
+
+static std::deque<std::deque<bool>> get_middle_subgrid(const std::deque<std::deque<bool>>& g_grid, const int width, const int height)
+{
+	const int half_w = static_cast<int>(g_grid[0].size()) / 2; // assume all are the same size
+	const int half_h = static_cast<int>(g_grid.size()) / 2;
+
+	std::deque<std::deque<bool>> r{};
+
+	for (int i = half_h - (height / 2); i < half_h + (height / 2); ++i)
+	{
+		std::deque<bool> w{};
+		for (int j = half_w - (width / 2); j < half_w + (width / 2); ++j)
+		{
+			w.push_back(g_grid[i][j]);
+		}
+		r.push_back(w);
+	}
+
+	return r;
+}
+
+
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(1920, 1080), "Game of Life");
 
-	window.setFramerateLimit(2);
+	window.setFramerateLimit(10);
 
 	// initalize the grid, using std::deque<bool> instead of std::vector<bool>
-	std::deque<bool> init;
-	std::deque<bool> init2;
-	std::deque<bool> init3;
-	std::deque<bool> empty;
-	for (int i = 0; i < 30; ++i)
+	std::deque<std::deque<bool>> grid(1000, std::deque<bool>(1000, false));
+	for (int i = 0; i < 20; ++i)
 	{
-		if (i < 10)
+		for (int j = 0; j < 20; ++j)
 		{
-			init.push_back(false);
-			init2.push_back(false);
-			init3.push_back(false);
+			grid[490 + i][490 + j] = random_bool();
 		}
-
-		init.push_back(random_bool());
-		init2.push_back(random_bool());
-		init3.push_back(random_bool());
 	}
-	std::deque<std::deque<bool>> grid{ empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, init, init2, init3 };
 
 	while (window.isOpen())
 	{
@@ -66,19 +78,6 @@ int main()
 
 		window.clear(sf::Color::Black);
 
-		// if the window was resized, resize the vectors
-		sf::Vector2u window_size = window.getSize();
-		unsigned int tile_size = 30;
-		unsigned int num_tiles_x = window_size.x / tile_size;
-		unsigned int num_tiles_y = window_size.y / tile_size;
-
-		grid.resize(num_tiles_y);
-		for (auto& row : grid)
-		{
-			row.resize(num_tiles_x);
-		}
-
-
 		// now run the rules of the game of life
 		// to apply all rules simultaniously, we must create an old board
 		auto old_grid = grid;
@@ -88,14 +87,14 @@ int main()
 			int xx = 0;
 			for (auto current_cell : row)
 			{
-				bool right = get_val_or_false(grid, yy, xx + 1);
-				bool left = get_val_or_false(grid, yy, xx - 1);
-				bool right_top = get_val_or_false(grid, yy + 1, xx + 1);
-				bool top = get_val_or_false(grid, yy + 1, xx);
-				bool left_top = get_val_or_false(grid, yy + 1, xx - 1);
-				bool bottom = get_val_or_false(grid, yy - 1, xx);
-				bool right_bottom = get_val_or_false(grid, yy - 1, xx + 1);
-				bool left_bottom = get_val_or_false(grid, yy - 1, xx - 1);
+				bool right = get_val_or_false(old_grid, yy, xx + 1);
+				bool left = get_val_or_false(old_grid, yy, xx - 1);
+				bool right_top = get_val_or_false(old_grid, yy + 1, xx + 1);
+				bool top = get_val_or_false(old_grid, yy + 1, xx);
+				bool left_top = get_val_or_false(old_grid, yy + 1, xx - 1);
+				bool bottom = get_val_or_false(old_grid, yy - 1, xx);
+				bool right_bottom = get_val_or_false(old_grid, yy - 1, xx + 1);
+				bool left_bottom = get_val_or_false(old_grid, yy - 1, xx - 1);
 
 				std::deque<bool> neighbors{ right, left, right_top, top, left_top, bottom, right_bottom, left_bottom };
 
@@ -121,10 +120,17 @@ int main()
 			++yy;
 		}
 
+		// if the window was resized, resize the vectors
+		sf::Vector2u window_size = window.getSize();
+		unsigned int tile_size = 30;
+		unsigned int num_tiles_x = window_size.x / tile_size;
+		unsigned int num_tiles_y = window_size.y / tile_size;
+
+		auto visible_grid = get_middle_subgrid(grid, static_cast<int>(num_tiles_x), static_cast<int>(num_tiles_y));
 
 		for (unsigned int y = 0; y < num_tiles_y; ++y) {
 			for (unsigned int x = 0; x < num_tiles_x; ++x) {
-				if (grid[y][x]) {
+				if (visible_grid[y][x]) {
 					sf::RectangleShape tile(sf::Vector2f(static_cast<float>(tile_size - 1), static_cast<float>(tile_size - 1)));
 					tile.setPosition(static_cast<float>(x * tile_size), static_cast<float>(y * tile_size));
 					tile.setFillColor(sf::Color::White);
